c58b5065b03b2aa4407ce0b41413aa3e164f5a40
钮旭明 <a977527479@sina.com>
2016-09-20 08:56:45 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 class App extends React.Component {
-  getChildContext() {
-    return {muiTheme: getMuiTheme()};
-  }
   render () {
     return(
       <div>
@@ -13,9 +10,5 @@ class App extends React.Component {
     )
   }
 }
-
-App.childContextTypes = {
-  muiTheme: React.PropTypes.object.isRequired,
-};
 
 export default App;

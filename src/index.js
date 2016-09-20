bbb066aea34e42ad514e73b3bd81e2e450e0f0ba
钮旭明 <a977527479@sina.com>
2016-09-20 08:54:37 +0800

    upd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import {render} from 'react-dom';
 
 import App from './App.js';
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
 render(<App />,document.getElementById('root'));
